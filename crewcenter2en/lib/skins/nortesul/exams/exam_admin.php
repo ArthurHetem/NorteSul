f79f9634d0bbf6f98120238b2a2d17ca9c7cd3e2
siddasilva <41838663+siddasilva@users.noreply.github.com>
2018-12-09 20:45:23 -0500

    Update exam_admin.php

--- crewcenter2en/lib/skins/nortesul/exams/exam_admin.php
+++ crewcenter2en/lib/skins/nortesul/exams/exam_admin.php
@@ -19,7 +19,7 @@
 		   <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Exams Administration</h3>
+              <h3 class="box-title">Exam Administration</h3>
             </div>
             <!-- /.box-header -->
 			<div class="box-body table-responsive">
@@ -29,29 +29,29 @@
         if (isset($message)) {echo '<tr><td colspan="6">'.$message.'</td></tr>';}
         ?>
         <tr>
-            <td colspan="6" bgcolor="#cccccc"><b>Exams Awaiting Approvation:</b></td>
+            <td colspan="6" bgcolor="#cccccc"><b>Exams Waiting for Approval:</b></td>
         </tr>
 
         <?php
         if (!$unapproved) {echo '<tr><td colspan="6" class="badge bg-red">No Exams.</td></tr>';}
         else {
             echo '<tr>
-                    <td colspan="4" align="right">Approved on the Exam</td>
+                    <td colspan="4" align="right">Exam Approved</td>
                     <td colspan="1"><div id="success">Score</div></td>
                     <td rowspan="2">Manage Exam<br />Submissions</td>
                 </tr>
                 <tr>
-                    <td colspan="4" align="right">Reprovado no Exame</td>
+                    <td colspan="4" align="right">Failed Exam</td>
                     <td colspan="1"><div id="error">Score</div></td>
 
                 </tr>
                 <tr>
-                    <td>Piloto</td>
-                    <td>Data</td>
-                    <td>Titulo do Exame</td>
-                    <td>Versão</td>
-                    <td>Porcentagem de Acertos</td>
-                    <td>Aprovar / Reprovar</td>
+                    <td>Pilot</td>
+                    <td>Date</td>
+                    <td>Exam Title</td>
+                    <td>Version</td>
+                    <td>Percentage of Right Answers</td>
+                    <td>Approved / Failed</td>
                 </tr>';
 
             foreach ($unapproved as $awaiting) {
@@ -74,16 +74,16 @@
         }
         ?>
         <tr>
-            <td colspan="6" bgcolor="#cccccc"><b>Pedidos de Exames:</b></td>
+            <td colspan="6" bgcolor="#cccccc"><b>Exams Inquired:</b></td>
         </tr>
 
         <?php
-        if (!$requests) {echo '<tr><td colspan="6" class="badge bg-red">Não existem pedidos de Exame no momento!</td></tr>'; }
+        if (!$requests) {echo '<tr><td colspan="6" class="badge bg-red">There are no Exams inquire at the moment!</td></tr>'; }
         else {
             echo '<tr>';
-            echo '<td colspan="2">Piloto</td>';
-            echo '<td colspan="2">Exame</td>';
-            echo '<td colspan="2">Aprovar pedido</td>';
+            echo '<td colspan="2">Pilot</td>';
+            echo '<td colspan="2">Exam</td>';
+            echo '<td colspan="2">Inquire Approval</td>';
             echo '</tr>';
 
             foreach ($requests as $request) {
@@ -92,20 +92,20 @@
                 echo '<tr>';
                 echo '<td colspan="2">'.$pilot->firstname.' '.$pilot->lastname.' '.PilotData::GetPilotCode($pilot->code, $request->pilot_id).'</td>';
                 echo '<td colspan="2">'.$exam->exam_description.'</td>';
-                echo '<td colspan="2"><a href="'.SITE_URL.'/index.php/Exams_admin/assign_exam_admin?id='.$exam->id.'&pilot_id='.$request->pilot_id.'" class="badge bg-green">Aprovar pedido</a></td>';
+                echo '<td colspan="2"><a href="'.SITE_URL.'/index.php/Exams_admin/assign_exam_admin?id='.$exam->id.'&pilot_id='.$request->pilot_id.'" class="badge bg-green">Inquire Approval</a></td>';
                 echo '</tr>';
             }
         }
         ?>
         <tr>
-            <td colspan="6" bgcolor="#cccccc"><b>Exames Assignados Atualmente:</b></td>
+            <td colspan="6" bgcolor="#cccccc"><b>Exams Taken:</b></td>
         </tr>
 <?php
 $assigned = ExamsData::assigned_exams();
-if (!$assigned) {echo '<tr><td colspan="6" class="badge bg-red">Nenhum Exame Designado no Momento</td></tr>'; }
+if (!$assigned) {echo '<tr><td colspan="6" class="badge bg-red">No Exam Assigned at the moment!</td></tr>'; }
         else {      echo '<tr>';
             echo '<td colspan="3">Piloto</td>';
-            echo '<td colspan="3">Exame Designado</td>';
+            echo '<td colspan="3">Taken Exam</td>';
             echo '</tr>';
 
             foreach ($assigned as $assign) {
@@ -122,39 +122,39 @@ if (!$assigned) {echo '<tr><td colspan="6" class="badge bg-red">Nenhum Exame Des
     <hr /><br />
     <table class="table table-bordered table-hover">
         <tr>
-            <td bgcolor="#cccccc"><b>Configuração de Exames e de Questões:</b></td>
-            <td bgcolor="#cccccc"><b>Ver/Editar</b></td>
+            <td bgcolor="#cccccc"><b>Exams and Questions Setup:</b></td>
+            <td bgcolor="#cccccc"><b>View/Edit</b></td>
         </tr>
         <tr>
-            <td>Exames na basde de dados: <?php $count=ExamsData::get_exam_count(); echo $count->total; ?></td>
-            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_current_exams" class="btn btn-flat btn-info">Editar/Ver Exames</a></td>
+            <td>Exams in database: <?php $count=ExamsData::get_exam_count(); echo $count->total; ?></td>
+            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_current_exams" class="btn btn-flat btn-info">Edit/View Exams</a></td>
         </tr>
         <tr>
-            <td>Questões na base de dados: <?php $count=ExamsData::get_question_count(); echo $count->total; ?></td>
-            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_current_questions" class="btn btn-flat btn-info">Editar/Ver Questões</a></td>
+            <td>Questions in database: <?php $count=ExamsData::get_question_count(); echo $count->total; ?></td>
+            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_current_questions" class="btn btn-flat btn-info">Edit/View Questions</a></td>
         </tr>
         <tr>
-            <td>Total de Exames Feitos por pilotos:</td>
+            <td>Total Exams taken by pilots:</td>
             <td><?php $totals = ExamsData::get_exams_taken();
 if ($totals[1] > 0) {echo $totals[1];}
-else {echo '<span class="badge bg-red">Nenhum</span>';}?>
+else {echo '<span class="badge bg-red">None</span>';}?>
             </td>
         </tr>
         <tr>
-            <td>Total de Exames Aprovados:</td>
+            <td>Total Exams Approved:</td>
             <td><?php
                 if ($totals[1] > 0) {echo $totals[0];}
-                else {echo '<span class="badge bg-red">Nenhum</span>';}?>
+                else {echo '<span class="badge bg-red">None</span>';}?>
             </td>
         </tr>
         <tr>
-            <td>Porcentagem de Aprovação nos Exames:</td>
+            <td>Percentage of Exames Approval:</td>
             <td><?php if ($totals[1] > 0) {$percent = (100 * ($totals[0] / $totals[1])); echo round($percent, 1).'%';}
-else {echo '<span class="badge bg-red">Nenhum</span>';}?>
+else {echo '<span class="badge bg-red">None</span>';}?>
             </td>
         </tr>
         <tr>
-            <td colspan="2"><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_individual_pilot"><b>Gerenciar Estátisticas individuais dos Pilotos</b></a></td>
+            <td colspan="2"><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/view_individual_pilot"><b>Manage Individual  Statistics by Pilots</b></a></td>
         </tr>
     </table>
     <hr /><br />
@@ -164,20 +164,20 @@ $admin = ExamsData::check_admin(Auth::$userinfo->pilotid);
                     ?>
     <table class="table table-bordered table-hover">
         <tr>
-            <td bgcolor="#cccccc"><b>Configuração do Centro de Exames:</b></td>
-            <td bgcolor="#cccccc"><b>Ver/Editar</b></td>
+            <td bgcolor="#cccccc"><b>Exam Center Settings:</b></td>
+            <td bgcolor="#cccccc"><b>View/Edit</b></td>
         </tr>
         <tr>
-            <td>Status do Centro de Exame: <?php
+            <td>Exam Center Status: <?php
     $setting = ExamsData::get_setting_info('2');
-        if ($setting->value == 0) { echo '<span class="badge bg-red">Fechado</span>'; }
-        else { echo '<span class="badge bg-green">Aberto</span>'; }
+        if ($setting->value == 0) { echo '<span class="badge bg-red">Closed</span>'; }
+        else { echo '<span class="badge bg-green">Open</span>'; }
         ?>
             </td>
-            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=2" class="btn btn-flat btn-info">Alterar Status do Centro de Exame</a></td>
+            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=2" class="btn btn-flat btn-info">Exam Center Status Change</a></td>
         </tr>
         <tr>
-            <td>Administradores do Centro de Exames:<br />
+            <td>Exam Center Administrators:<br />
     <?php
     $admins = ExamsData::get_admin('1');
     foreach($admins as $admin) {
@@ -188,10 +188,10 @@ $admin = ExamsData::check_admin(Auth::$userinfo->pilotid);
                     }
     ?>
             </td>
-            <td rowspan="2"><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/edit_admin_list" class="btn btn-flat btn-info">Adicionar/Remover<br />Administradores<br />& Membros do Staff</a></td>
+            <td rowspan="2"><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/edit_admin_list" class="btn btn-flat btn-info">Add/Remove<br />Administrators<br />& Staff Members</a></td>
         </tr>
         <tr>
-            <td>Membros do Staff do Centro de Exames:<br /><b><?php
+            <td>Exam Center Staff Members:<br /><b><?php
                     $authors = ExamsData::get_admin('2');
                     if (!$authors) {echo 'The Are No Staff Members Currently';}
                     else {
@@ -205,7 +205,7 @@ $admin = ExamsData::check_admin(Auth::$userinfo->pilotid);
             </td>
         </tr>
         <tr>
-            <td>Mensagem Quando o Centro de Exames está aberto: <?php
+            <td>When is the Exam Center Open: <?php
                         $setting = ExamsData::get_setting_info('4');
                         echo '<br /><b>'.$setting->value.'</b>';
                         ?>
@@ -213,21 +213,21 @@ $admin = ExamsData::check_admin(Auth::$userinfo->pilotid);
             <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=4" class="btn btn-flat btn-info">Editar Mensagem</a></td>
         </tr>
         <tr>
-            <td>Mensagem Quando o Centro de Exames está aberto: <?php
+            <td>Message When is the Exam Center open: <?php
                         $setting = ExamsData::get_setting_info('3');
                         echo '<br /><b>'.$setting->value.'</b>';
                         ?>
             </td>
-            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=3" class="btn btn-flat btn-info">Editar Mensagem</a></td>
+            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=3" class="btn btn-flat btn-info">Edit Message</a></td>
         </tr>
         <tr>
-            <td>Admin Designar Exames Ativado: <?php
+            <td>Admin Assigned Active Exames : <?php
     $setting = ExamsData::get_setting_info('5');
-                    if ($setting->value == 0) { echo '<span class="badge bg-red">Não</span>'; }
+                    if ($setting->value == 0) { echo '<span class="badge bg-red">No</span>'; }
                     else { echo '<span class="badge bg-green">Sim</span>'; }
                     ?>
             </td>
-            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=5" class="btn btn-flat btn-info">Editar Designação dos Admins</a></td>
+            <td><a href="<?php echo SITE_URL ?>/index.php/Exams_admin/get_setting_info?id=5" class="btn btn-flat btn-info">Edit Admins Assignements</a></td>
         </tr>
     </table>
     <hr />
@@ -239,15 +239,15 @@ $admin = ExamsData::check_admin(Auth::$userinfo->pilotid);
         <tr>
             <td>
                 <form method="link" action="<?php echo SITE_URL ?>/index.php/Exams_admin/new_test_form">
-                    <input type="submit" class="btn btn-flat btn-info" value="Criar Novo Exame"></form>
+                    <input type="submit" class="btn btn-flat btn-info" value="Generate New Exam"></form>
             </td>
             <td>
                 <form method="link" action="<?php echo SITE_URL ?>/index.php/Exams_admin/new_question_form">
-                    <input type="submit" class="btn btn-flat btn-info" value="Criar Nova Questão"></form>
+                    <input type="submit" class="btn btn-flat btn-info" value="Generate New Questions"></form>
             </td>
             <td>
                 <form method="link" action="<?php echo SITE_URL ?>/index.php/Exams_admin/new_revision_form">
-                    <input type="submit" class="btn btn-flat btn-info" value="Criar Rasão para Revisão"></form>
+                    <input type="submit" class="btn btn-flat btn-info" value="generate Reson for Review"></form>
             </td>
         </tr>
     </table>
