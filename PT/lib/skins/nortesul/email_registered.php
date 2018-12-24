1a9b4f0d9ad6bd8b2ca58d1066c208df745c7531
ArthurHetem <arthurhetemgames@gmail.com>
2018-12-24 10:52:52 -0200

    Update email_registered.php

--- PT/lib/skins/nortesul/email_registered.php
+++ PT/lib/skins/nortesul/email_registered.php
@@ -1,7 +1,7 @@
 <?php if(!defined('IN_PHPVMS') && IN_PHPVMS !== true) { die(); } ?>
 Caro <?php echo $firstname .' '. $lastname; ?>,
 
-Seu Cadastro na NorteSul foi efetuado, Porém sua conta ainda passará por uma analise. Você recebera um e-mail em breve, com a resposta de seu cadastro.
+Seu Cadastro na NorteSul foi efetuado, Porém sua conta ainda passará por uma analise. Você receberá um e-mail em breve, com a resposta de seu cadastro.
 
 
 Atenciosamente,
